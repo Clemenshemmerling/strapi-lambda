55a2e1dde7873035d908b4197431c90411e97495
Clemens Hemmerling <clemens@getkor.io>
2021-02-09 14:13:31 -0600

    test

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const strapiInstance = startStrapi();
 let strapiStartPromise = null;
 
 module.exports.handler = async (event, context) => {
-  console.log("test")
-  // if (strapiStartPromise === null) {
-  //   strapiStartPromise = strapiInstance.start();
-  // }
-  // await strapiStartPromise;
-  // const handler = serverless(strapiInstance.app);
-  // return handler(event, context);
+  if (strapiStartPromise === null) {
+    strapiStartPromise = strapiInstance.start();
+  }
+  await strapiStartPromise;
+  const handler = serverless(strapiInstance.app);
+  return handler(event, context);
 };
